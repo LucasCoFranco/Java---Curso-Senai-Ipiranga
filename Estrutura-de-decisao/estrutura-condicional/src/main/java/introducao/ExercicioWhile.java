c0b3957dc9b0fbdea6f50a2e8558a540b2050964
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-07-06 17:22:45 -0300

    Update ExercicioWhile.java

--- Estrutura-de-decisao/estrutura-condicional/src/main/java/introducao/ExercicioWhile.java
+++ Estrutura-de-decisao/estrutura-condicional/src/main/java/introducao/ExercicioWhile.java
@@ -14,3 +14,23 @@ public class Main {
         }
     }
 }
+
+/**==============================================================================================================
+Outra maneira de fazer*//
+//!!!!IMPORTATE!!!!!!!!!
+// o do while não aceita comparações do tipo == ou !=, então para querer utilizar nessas condições, será necessário usar ! ou equals
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String nome;
+        Scanner sc = new Scanner(System.in);
+            do{
+                System.out.print("Digite o seu nome: ");
+                nome = sc.nextLine().toLowerCase();
+                System.out.printf("Seja Bem-Vindo %s fico feliz por ter voce aqui\n",nome);
+            } while (!nome.equals("sair"));
+
+        }
+    }
+
+//não recomendado, pois ele falará seja bem-vindo ao sair 
